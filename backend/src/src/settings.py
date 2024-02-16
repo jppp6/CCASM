98aaab518cdefc45fe82c8e52fc6e2a77c8d713f
Alex Baldassare <18cab16@hs-ccasm-d-w01.ad.azure.queensu.ca>
2024-02-16 18:20:59 -0500

    Changed dotenv function

--- backend/src/src/settings.py
+++ backend/src/src/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from os import getenv
-from dotenv import load_doatenv
+from dotenv import load_dotenv
 
-load_doatenv()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
