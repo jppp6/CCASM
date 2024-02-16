b633c0a1579f33e2783e6c9e04762e850fa4b746
jp <6jpleblanc@gmail.com>
2024-02-16 17:40:22 -0500

    ccasm14: information sheet, both searchs and csv output

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComplexSearchComponent } from './components/complex-search/complex-search.component';
+import { SimpleSearchComponent } from './components/simple-search/simple-search.component';
+import { StrainDetailsDialog } from './components/strain-details/strain-details.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { HeaderComponent } from './core/header/header.component';
+import { ToTitleCasePipe } from './core/utils/titlecase.pipe';
 import { AboutComponent } from './pages/about/about.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { BrowseComponent } from './pages/browse/browse.component';
@@ -20,18 +16,25 @@ import { HomeComponent } from './pages/home/home.component';
 import { PageNotFoundComponent } from './pages/notfound/notfound.component';
 import { StatisticsComponent } from './pages/statistics/statistics.component';
 
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTreeModule } from '@angular/material/tree';
-import { ComplexSearchComponent } from './components/complex-search/complex-search.component';
-import { SimpleSearchComponent } from './components/simple-search/simple-search.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -49,6 +52,8 @@ import { SimpleSearchComponent } from './components/simple-search/simple-search.
     AboutComponent,
     ComplexSearchComponent,
     SimpleSearchComponent,
+    StrainDetailsDialog,
+    ToTitleCasePipe,
   ],
   imports: [
     BrowserModule,
@@ -57,6 +62,7 @@ import { SimpleSearchComponent } from './components/simple-search/simple-search.
     MatButtonModule,
     MatIconModule,
     MatSelectModule,
+    MatDialogModule,
     MatToolbarModule,
     MatTreeModule,
     FormsModule,
