0cf38c8b0a4f048b6073e32e358c682228e68860
Marshall <19ba1@queensu.ca>
2024-02-27 03:17:08 -0500

    Updated Contact Page
    Used a new layout and deleted abundant part as requested.

--- frontend/src/app/pages/contact/contact.component.ts
+++ frontend/src/app/pages/contact/contact.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  admins = [
+    { name: 'Admin Name', 
+    email: 'admin_email@queensu.ca', 
+    phone: '+1 234 567 8901', 
+    photoPath: 'path_to_admin_photo.jpg' 
+  },
+    { name: 'Admin Name', 
+    email: 'admin_email@queensu.ca', 
+    phone: '+1 234 567 8901', 
+    photoPath: 'path_to_admin_photo.jpg' 
+  },
+  ];
 
 }
