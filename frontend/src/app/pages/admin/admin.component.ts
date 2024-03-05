dabb488bf06b00aa575afe87b1f91459afb64075
julian-wei213 <julianwei@hotmail.ca>
2024-03-05 01:22:41 -0500

    base functions of admin page

--- frontend/src/app/pages/admin/admin.component.ts
+++ frontend/src/app/pages/admin/admin.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject} from '@angular/core';
+import { CCASMService } from '../../core/services/ccasm.services';
+import { AdminAccount } from 'src/app/core/utils/ccasm.types';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  })
+
+  services = inject(CCASMService)
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm.valueChanges.subscribe(console.log);
+  }
+
+  login() {
+    // TODO validate password and authenticate session token
+
+    const loginReq: AdminAccount = {
+      adminId: 1,
+      email: this.loginForm.controls.email.value!,
+      password: this.loginForm.controls.password.value!,
+      creationDate: new Date()
+    }
+
+    this.services.postAccount(loginReq).subscribe({
+      // TODO do something with the output
+      // Success
+      next: () => {
+        console.log("success");
+      },
+      // Failure
+      error: (error) => {
+        console.log("failed", error);
+      }
+    })
+  }
 
 }
