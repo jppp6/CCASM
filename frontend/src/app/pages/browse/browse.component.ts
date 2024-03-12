7bbb4b5c39219c746cfa9f502348c57a88d291c3
jp <6jpleblanc@gmail.com>
2024-03-12 10:32:35 -0400

    fixed strain tree

--- frontend/src/app/pages/browse/browse.component.ts
+++ frontend/src/app/pages/browse/browse.component.ts
@@ -48,6 +48,10 @@ export class BrowseComponent implements OnInit {
                 result.strains
             ) as Strain[];
 
+            this.treeDataSource.data = this.buildTree(
+                this.allStrains.map((s) => s.taxonomicLineage)
+            );
+
             this.simpleOptions = Utils.filterDuplicatesAndFalsy(
                 this.allStrains.map((s) => s.binomialClassification)
             );
