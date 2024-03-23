33306e41a3203aeb5e83ba83691a98dd41ec9fbe
julian-wei213 <julianwei@hotmail.ca>
2024-03-23 16:04:38 -0400

    registered model to admin

--- backend/src/strains/admin.py
+++ backend/src/strains/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Deposits, Requests, Strains
+ 
+admin.site.register(Deposits)
+admin.site.register(Requests)
+admin.site.register(Strains)
